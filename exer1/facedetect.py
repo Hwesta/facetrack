2b46b2036d1ddbc86a2252778125e44e2664a0b1
Holly Becker <becker.holly@gmail.com>
2018-01-16 19:40:09 -0800

    Do exercise 1

--- exer1/facedetect.py
+++ exer1/facedetect.py
@@ -1,29 +1,34 @@
-'''
-Q1: within ipython do
-    run facedetect.py group_photo_large.jpg
-    run facedetect.py group_photo_small.jpg
-    how are the results different?
+#!/usr/bin/env python
+# Q1: within ipython do
+#     run facedetect.py group_photo_large.jpg
+#     run facedetect.py group_photo_small.jpg
+#     how are the results different?
 
-Q2: what happens if you adjust the value of the scaleFactor parameter passed to faceCascade.detectMultiScale?
-Q3: modify find_faces to check if the image is already grey-scale
-Q4: after you run find_faces, use cv2.imshow() to view the contents of 'image'.... did the function add green squares? Modify find_faces so that it does not change the original image.
-Q5: What are the current dimensions of the image? Which dimension is the height and which dimension is the width? Add a line to __main__ that prints the image dimensions.
-Q6: modify find_faces so that the image shown doesn't have a dimension larger than 800 pixels. Do you want to resize the image before or after the face detection routine runs?
+# Q2: what happens if you adjust the value of the scaleFactor parameter passed to faceCascade.detectMultiScale?
+# Q3: modify find_faces to check if the image is already grey-scale
+# Q4: after you run find_faces, use cv2.imshow() to view the contents of 'image'.... did the function add green squares? Modify find_faces so that it does not change the original image.
+# Q5: What are the current dimensions of the image? Which dimension is the height and which dimension is the width? Add a line to __main__ that prints the image dimensions.
+# Q6: modify find_faces so that the image shown doesn't have a dimension larger than 800 pixels. Do you want to resize the image before or after the face detection routine runs?
 
-'''
+
 import cv2
+import os
 import sys
 
 
-def find_faces(image, faceCascade):
+def find_faces(image, faceCascade, output_path, resize=False):
     # the haar cascade algorithm requires greyscale images as input
     # convert image to greyscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if image.shape[2] >= 1:
+        # make greyscale
+        grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        grey = image
 
     # Detect faces in the image
     faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
+        grey,
+        scaleFactor=1.2,
         minNeighbors=5,
         minSize=(30, 30)
         # flags = cv2.CV_HAAR_SCALE_IMAGE
@@ -35,9 +40,26 @@ def find_faces(image, faceCascade):
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    cv2.imshow("Faces found", image)
-    input('press a key to continue')
-    cv2.destroyAllWindows()
+    # Resize so never bigger than 800 px
+    if resize:
+        x_size = image.shape[1]
+        y_size = image.shape[0]
+        if x_size > y_size:
+            new_x = 800
+            ratio = new_x / x_size
+            new_y = int(y_size * ratio)
+        else:
+            new_y = 800
+            ratio = new_y / y_size
+            new_x = int(x_size * ratio)
+
+        print('old size', x_size, y_size, 'new size', new_x, new_y, 'ratio', ratio)
+
+        resized = cv2.resize(image, (new_x, new_y))
+    else:
+        resized = image
+
+    cv2.imwrite(output_path, resized)
 
 
 def getCasc():
@@ -56,9 +78,11 @@ if __name__ == "__main__":
     imagePath = sys.argv[1]
 
     image = cv2.imread(imagePath)
+    print('Image dimensions:', image.shape[1], 'x', image.shape[0])
     faceCascade = getCasc()
+    output_path = '.out'.join(os.path.splitext(imagePath))
 
-    find_faces(image, faceCascade)
+    find_faces(image, faceCascade, output_path)
 
     # max_x, max_y = 800, 800
     # yscale = min([max_y/image.shape[0], 1.0])
