88246f33d78bd7eb0c0951e19f68d559875cd14b
Holly Becker <becker.holly@gmail.com>
2018-01-16 18:21:51 -0800

    Fix linting, tabs to space

--- exer2/facetrack.py
+++ exer2/facetrack.py
@@ -1,51 +1,40 @@
 import cv2
-import sys
 
 
 def find_faces(faceCascade):
+    video_capture = cv2.VideoCapture(0)
 
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
 
-	video_capture = cv2.VideoCapture(0)
-	
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	while True:
-		# Capture frame-by-frame
-		ret, frame = video_capture.read()
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
 
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-		faces = faceCascade.detectMultiScale(
-			gray,
-			scaleFactor=1.1,
-			minNeighbors=5,
-			minSize=(30, 30)
-		)
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
 
-		# Draw a rectangle around the faces
-		for (x, y, w, h) in faces:
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-		# Display the resulting frame
-		cv2.imshow('Video', frame)
-
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-
-	# When everything is done, release the capture
-	video_capture.release()
-	cv2.destroyAllWindows()
-
-
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-	cascPath = 'haarcascade_frontalface_default.xml'
-	faceCascade = cv2.CascadeClassifier(cascPath)
+    cascPath = 'haarcascade_frontalface_default.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
 
-	print('to stop capture make the video window the foreground window and press any key ')
-	find_faces(faceCascade)
-	
-	
-	
-	
-	
+    print('to stop capture make the video window the foreground window and press any key ')
+    find_faces(faceCascade)
